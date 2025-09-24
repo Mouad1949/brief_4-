f097bbfe2fcbfa1875851ade13aa9e181b268c2a
aminahabab <amina.habab@gmail.com>
2025-09-24 16:00:59 +0100

    Ajout du composant BlocRecherche (barre de recherche)

--- src/App.jsx
+++ src/App.jsx
@@ -7,23 +7,26 @@ import Favoris from "./pages/Favoris";
 import APropos from "./pages/APropos";
 import AdminDashboard from "./Pages/Admin";
 import Footer from "./Components/layout/Footer";
-
 export default function App() {
   return (
-    <Router>
-      <Navbar />
-      <main className="container mx-auto px-4 py-6">
-        <Routes>
-          <Route path="/" element={<Accueil />} />
-          <Route path="/publier" element={<Publier />} />
-          <Route path="/favoris" element={<Favoris />} />
-          <Route path="/a-propos" element={<APropos />} />
-          <Route path="/admin" element={<AdminDashboard />} />
-        </Routes>
-      </main>
-    </Router>
+    <>
+      <Router>
+        <Navbar />
+        <main className="container mx-auto px-4 py-6">
+          <Routes>
+            <Route path="/" element={<Accueil />} />
+            <Route path="/publier" element={<Publier />} />
+            <Route path="/favoris" element={<Favoris />} />
+            <Route path="/a-propos" element={<APropos />} />
+            <Route path="/admin" element={<AdminDashboard />} />
+          </Routes>
+        </main>
+      </Router>
+      <Footer />
+    </>
   );
 }
+
 
 
 
