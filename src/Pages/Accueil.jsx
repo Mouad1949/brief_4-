520195dbbdf3b511b144042a2aa61e77bbfbb5e2
mouad <mouadlaalam6@gmail.com>
2025-09-25 11:53:44 +0100

    change

--- src/Pages/Accueil.jsx
+++ src/Pages/Accueil.jsx
@@ -3,12 +3,16 @@ import React from "react";
 import ComponentBanner from "../Components/ComponentBanner";
 import BlocRecherche from "../Components/BlocRecherche";
 import DerniersAjouts from "../Components/DerniersAjouts";
+import Categories from "../Components/Categories";
+import Oeuvres from "../Components/Oeuvres";
 export default function Accueil() {
   return (
     <div>
       <ComponentBanner />
       <BlocRecherche />
       <DerniersAjouts />
+      <Categories />
+      <Oeuvres />
     </div>
   );
 }
